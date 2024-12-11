4c345ec4904b2610fed3c49bc1e6a696b5dba27a
dfmalexi <emrea388@gmail.com>
2024-12-11 21:13:50 +0300

    CustomExceptionFilter created.

--- Api/Filters/CustomExceptionFilter.cs
+++ Api/Filters/CustomExceptionFilter.cs
@@ -7,32 +7,67 @@ namespace Api.Filters
 {
     public class CustomExceptionFilter : IExceptionFilter
     {
+        private readonly IDictionary<Type, Action<ExceptionContext>> _exceptionHandlers;
+
+        public CustomExceptionFilter()
+        {
+            _exceptionHandlers = new Dictionary<Type, Action<ExceptionContext>>
+            {
+                { typeof(BadRequestException), HandleBadRequestException },
+                { typeof(NotFoundException), HandleNotFoundException },
+                // Yeni istisnalar buraya eklenebilir
+            };
+        }
+
         public void OnException(ExceptionContext context)
         {
-            if (context.Exception is BadRequestException badRequestException)
+            Type exceptionType = context.Exception.GetType();
+            if (_exceptionHandlers.ContainsKey(exceptionType))
             {
-                badRequestException.Data["StackTrace"] = null;
-                context.Result = new BadRequestObjectResult(new
-                {
-                    ExceptionErrorMessage = badRequestException.Message,
-                    ValidationErrorMessage = badRequestException.ValidationErrors
-                })
-                {
-                    StatusCode = 400
-                };
-                context.ExceptionHandled = true;
+                _exceptionHandlers[exceptionType].Invoke(context);
             }
             else
             {
-                context.Result = new JsonResult(new
-                {
-                    error = "Internal Server Error",
-                    details = context.Exception.Message
-                })
-                {
-                    StatusCode = 500
-                };
+                HandleUnknownException(context);
             }
+        }
+
+        private void HandleBadRequestException(ExceptionContext context)
+        {
+            var badRequestException = (BadRequestException)context.Exception;
+            badRequestException.Data["StackTrace"] = null;
+            context.Result = new BadRequestObjectResult(new
+            {
+                message = badRequestException.ValidationErrors?.ToString() ??  badRequestException.Message ,
+            })
+            {
+                StatusCode = 400
+            };
+            context.ExceptionHandled = true;
+        }
+        private void HandleNotFoundException(ExceptionContext context)
+        {
+            var notFoundException = (NotFoundException)context.Exception;
+            notFoundException.Data["StackTrace"] = null;
+            context.Result = new NotFoundObjectResult(new
+            {
+                message = notFoundException.Message,
+            })
+            {
+                StatusCode = 404
+            };
+            context.ExceptionHandled = true;
+        }
+        private void HandleUnknownException(ExceptionContext context)
+        {
+            context.Result = new JsonResult(new
+            {
+                message = context.Exception.Message,
+                stackTrace = context.Exception.StackTrace,
+            })
+            {
+                StatusCode = 500
+            };
         }
     }
 }
