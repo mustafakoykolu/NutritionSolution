0ec45c04f340485ef15dc1b46cbbe4d4d522b959
dfmalexi <emrea388@gmail.com>
2024-12-29 14:46:47 +0300

    nullable added to properties

--- Application/Features/Meals/Dtos/MealDto.cs
+++ Application/Features/Meals/Dtos/MealDto.cs
@@ -5,10 +5,10 @@ namespace Application.Features.Meals.Dtos
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } // Yemek açıklaması
         public string Recipe { get; set; } = string.Empty;
-        public string ImagePath { get; set; } = string.Empty;
-        public string History { get; set; } = string.Empty;
+        public string? ImagePath { get; set; }
+        public string? History { get; set; } 
+        public string? Benefits { get; set; } 
         public NutrientDto TotalNutrients { get; set; }
         public List<MealFoodDto> MealFoods { get; set; } // GetFoods'ta kullanılan DTO
     }
--- Application/Features/Meals/Dtos/NutrientDto.cs
+++ Application/Features/Meals/Dtos/NutrientDto.cs
@@ -1,12 +1,20 @@
-﻿namespace Application.Features.Meals.Dtos
+﻿using Domain.Entity;
+
+namespace Application.Features.Meals.Dtos
 {
     public class NutrientDto
     {
-        public float KCal { get; set; }
-        public float Protein { get; set; }
-        public float Fat { get; set; }
-        public float Carbohydrate { get; set; }
-        public float Fiber { get; set; }
-        public float Sugar { get; set; }
+        public int Id { get; set; }
+        public float? KCal { get; set; }
+        public float? Protein { get; set; }
+        public Lipid Fat { get; set; }
+        public Carbohydrate Carbohydrate { get; set; }
+        public float? Water { get; set; }
+        public float? Nitrogen { get; set; }
+        public float? Portion { get; set; }
+        public string? PortionUnit { get; set; }
+        public float? Caffeine { get; set; }
+        public Vitamin Vitamin { get; set; }
+        public Mineral Mineral { get; set; }
     }
 }
